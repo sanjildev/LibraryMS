fe0842cfb49a2f098e14ac4f23806ec2bddf1a9c
sanjildev <shakyasanjil00@gmail.com>
2025-04-01 09:08:25 +0545

    update image

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express=require('express')
+const fs=require('fs')
 const connectToDatabase = require('./database')
 const Book = require('./model/bookModel')
 
 //multerconfig imports
 
 const {multer,storage}=require('./middleware/multerConfig')
-const upload=multer({storage:storage})
+const upload=multer({storage:storage,limits:{fileSize:1000000}})
 const app=express()
 app.use(express.json())  //hamile postman bata req pathuada node js le bujna sakdiana so tei vayer node js lai kura bujauna ko lagi hamile app.use(express.json()) vanni euta code lekhnu parx ani yo bahira bata frontend aauda use garinx like client side bata
 
@@ -24,11 +25,21 @@ app.get('/home',(req,res)=>{
 
 
 //create books
-app.post('/book',upload.single('image'),async(req,res)=>{
+app.post('/book',upload.single('imageUrl'),async(req,res)=>{
  const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication}=req.body
+let fileName;
+if (!req.file){
+    fileName="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+}
+else{
+    fileName="http://localhost:5000/" + req.file.filename
+}
+
+ console.log(req.file);
+
    await Book.create({
         bookName,bookPrice,isbnNumber,authorName,publishedAt,publication,
-        imageUrl:req.file.filename
+        imageUrl:fileName
     })
     res.status(201).json({ //201--> created vaner janaux
         message:"book created  successfully"
@@ -81,11 +92,31 @@ app.delete('/book/:id',async(req,res)=>{
 
 
  //update book
- app.patch('/book/:id',async(req,res)=>{
+ app.patch('/book/:id',upload.single('imageUrl'),async(req,res)=>{
 const id=req.params.id
 const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication}=req.body
+const oldDatas=await Book.findById(id)
+let fileName;
+if(req.file){
+    const oldImagePath=oldDatas.imageUrl
+   console.log(oldImagePath);
+   const localhostUrlLength="http://localhost:5000/".length
+   const newOldImagePath=oldImagePath.slice(localhostUrlLength)   
+   fs.unlink(`storage/${newOldImagePath}`,(err)=>{ //unlink ko kaam vaneko file delte garne 
+    if(err){
+        console.log(err);
+        
+    }
+    else{
+        console.log('file deleted successfully');
+        
+    }
+   })
+   fileName="http://localhost:5000/" + req.file.filename
+}
+
 await Book.findByIdAndUpdate(id,{
-    bookName,bookPrice,isbnNumber,authorName,publishedAt,publication
+    bookName,bookPrice,isbnNumber,authorName,publishedAt,publication,imageUrl:fileName
 })
 
 
--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -1,7 +1,9 @@
 const multer=require('multer')
 const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        const allowedFileTypes=['image/png','image/jpeg','image/jpg'] //yo line of code le chai k vanx vanda kheri kasto format ko file haru chai accept hunc vancha
+        const allowedFileTypes=['image/png','image/jpeg','image/jpg'] //yo line of code le chai k vanx vanda kheri kasto format ko file haru chai accept hunx vancha
+console.log(req.file);
+
         if(!allowedFileTypes.includes(file.mimetype)){
             cb(new Error('This file is not supported!!')) //cb(error)
             return //yesle le chai yedi condition milyo vane terminate handinx
@@ -9,6 +11,7 @@ const storage=multer.diskStorage({
 cb(null,'./storage')
     },
     filename:function(req,file,cb){
+        
 cb(null,Date.now()+ '-'+ file.originalname)
     }
 })
